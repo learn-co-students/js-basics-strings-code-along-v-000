8c2db15420e6d5bc8cb3d5480f9fd19c299a2612
halfields <hfields697@aol.com>
2017-09-29 20:20:18 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Hal Fields";
+let welcomeMessage = "Welcome to Flatbook, Hal Fields!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, HAL FIELDS!";
+let shortGreeting = "Welcome, H!"
+
+
