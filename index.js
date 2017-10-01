02e3f6a82bce8cbe8fd4a00a546ad79cd6568748
kromoser <kevinromoser@gmail.com>
2017-10-01 15:46:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Kevin Romoser';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ' + currentUser.slice(0,1) + '!';
