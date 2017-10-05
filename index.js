0542cb34c39445e570d80edd937ff78fdb4ed3f7
dustin <dandersonsoftware@outlook.com>
2017-10-04 21:52:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Snickers';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCaser();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`
