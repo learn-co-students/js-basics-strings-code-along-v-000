5f68b52e63500427c5e6541d0085abc35a9bc147
Bartek Swistak <bartekswistak24@gmail.com>
2017-10-14 23:35:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Bartek';
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + '!';
+// For some reason I could not get ${currentUser} to run correctly
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
