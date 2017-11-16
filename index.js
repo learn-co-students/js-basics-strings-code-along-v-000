682895ee5ac4b8467ddc8ade5851118dc3495c6d
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2017-11-16 15:13:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+let currentUser = "Ricardo Colon Medina";
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `${welcomeMessage.split(" ", 1)[0]}, ${currentUser[0]}!`;
+
