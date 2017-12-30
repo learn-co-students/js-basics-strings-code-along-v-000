320d0f13811f193c4756f33943095463fbdff65a
Pat  Pignataro <pat6284@msn.com>
2017-12-30 21:05:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+currentUser = 'Pat';
+welcomeMessage = 'Welcome to Flatbook, Pat!';
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK, PAT!';
+shortGreeting = 'Welcome, P!';
