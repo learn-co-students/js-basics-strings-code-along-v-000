1641eca1519f224f987981f0a8f8752cef0a391d
Joseph Hyatt <github email address>
2018-01-23 14:27:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Joseph Hyatt';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
