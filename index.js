14378982cc6b22fab210f3e9b2c732daf6888e5d
Moshe Levi <mo6709@gmail.com>
2018-02-15 16:18:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+
+let currentUser = "Moishy";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase() + currentUser + "!";
+
+let shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+
+
+
+
