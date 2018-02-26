0a8d5a52309c5a1cc7ce144586014003084ffe70
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-26 15:29:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Context.it';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
