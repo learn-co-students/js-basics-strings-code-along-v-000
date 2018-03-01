0091e7753ca6a35792bab0111e8359c9ae2e19d5
Morgan Fudge <mfudge92@gmail.com>
2018-03-01 14:53:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+currentUser = 'currentUser'
+
+welcomeMessage =  `Welcome to Flatbook, ${currentUser}!`
+
+excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+
+shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
