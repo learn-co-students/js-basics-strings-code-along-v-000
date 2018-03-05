ce4d90f46cf7097136a21e2e097417d6afe297ee
Sam Marshall <samuel.r.marshall@gmail.com>
2018-03-04 19:24:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+currentUser = "Sam"
+welcomeMessage ="Welcome to Flatbook, "+currentUser+"!"
+excitedWelcomeMessage = welcomeMessage.toUpperCase()
+shortGreeting = "Welcome, "+currentUser.charAt()+"!"
