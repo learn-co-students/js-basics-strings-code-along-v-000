34235c5ed22ea2fbaa7be2c62f5a795a005daf68
Olena Ageyeva <eageeva@hotmail.com>
2018-04-05 11:59:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your code in this file!
-const currentUser='Olena'
-const welcomeMessage='Welcome to Flatbook, currentUser';
+const currentUser='Olena Ageyeva'
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage=welcomeMessage.toUpperCase();
+const shortGreeting=`Welcome, ${currentUser.charAt(0)}!`;
