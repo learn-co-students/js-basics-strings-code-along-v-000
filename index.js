6d2427b68cf29586317efb25d07a6e30cc40a6b7
blinded93 <blinded93@gmail.com>
2018-04-04 17:25:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'Kyle Hughes';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+cs-variables-lab-v-000
