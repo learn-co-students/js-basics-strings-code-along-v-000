faf91017f4405c18b5351069a6ad1c9a7b327f39
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-12 20:58:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Efrain';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
