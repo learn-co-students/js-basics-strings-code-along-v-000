90416efa1029837a3540aa2bd60e6131e035d69a
mcclains2003 <mcclains2003@gmail.com>
2018-04-16 15:45:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+var currentUser = "Shaun";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
