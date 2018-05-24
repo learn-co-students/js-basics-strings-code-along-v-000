03d6b3e73aeba6dc251b7fabcf0f0f851cedbaa3
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-05-24 11:52:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Andrew Hong";
+
+// const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
