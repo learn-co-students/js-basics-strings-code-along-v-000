733cad8ea3f44a32541f1c40f539b04b31165960
Richard Steinbach <steinbach.dick@gmail.com>
2018-05-31 12:32:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+let currentUser = 'SALLY';
+
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
+
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
