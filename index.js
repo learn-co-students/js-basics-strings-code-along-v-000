bc1436b3798c46f2892019d28be2412907d9d13e
anthonygharvey <anthony@anthonygharvey.com>
2018-06-21 00:12:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Anthony";
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, " + currentUser.charAt(0) + '!';
