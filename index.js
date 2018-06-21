b9a0a6f869d80cf357f4a74bd500d085d7ce569f
anthonygharvey <anthony@anthonygharvey.com>
2018-06-21 00:20:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = "Anthony";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
