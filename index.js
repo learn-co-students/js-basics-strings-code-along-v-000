9836e03422730b13c50641be154f2436cc3bba9d
Michelle Olivieri <michelleo72@gmail.com>
2018-06-26 19:05:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper";
+
+// Concatenate
+//const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+// Template literal - uses backticks
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
