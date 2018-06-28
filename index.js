bdfb7eaa5c4d0813f45117954aa6fb77adb4760f
Dale Terrell <tragicid@gmail.com>
2018-06-28 14:59:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const currentUser = 'Dale Terrell';
-const welcomeMessage = "Welcome to Flatbook, currentUser"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
