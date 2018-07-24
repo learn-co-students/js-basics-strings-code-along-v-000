71ad4ef6cb356a72018ff6b59ae3aa6e16c94cea
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 15:48:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+var currentUser = "The 'currentUser' variable must contain a string"
+var welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+var excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+var shortGreeting = "Welcome, " + currentUser[0] + "!"
