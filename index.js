f20500f3edddae393f9ddab7286572350b6d9cbb
AAM77 <Adeel.A.Mhd@gmail.com>
2018-11-02 19:05:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
 let currentUser = "Brother";
 
-let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const welcomeMessage = `Welcome to Flatbook,  + ${currentUser} + !`;
 
-let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
 
-let shortGreeting = "Welcome, " + currentUser.substr(0,1).toUpperCase() + "!";
+const shortGreeting = `Welcome, ${currentUser.substr(0,1).toUpperCase()}!`;
