3372863b51cda57af43b2b2695499081e54d9293
Barbara Washington <barbara.m.washington@gmail.com>
2018-12-21 20:08:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Babs';
+const welcomeMessage = 'Welcome to Flatbook, Babs!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, BABS!';
+const shortGreeting = "Welcome, B!"
