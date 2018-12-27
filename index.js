71a4c3b56ed849078c22987d899b5e953ce57b07
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2018-12-27 17:11:47 -0500

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!"
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
 let shortGreeting = "Welcome, " + firstInitial + "!"
+
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
