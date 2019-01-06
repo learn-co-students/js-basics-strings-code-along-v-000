c8d8381f0c0aae3e3a474a4c2d49a0488e3aab9b
atxrenegade <harleighabel@gmail.com>
2019-01-05 22:45:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Harleigh Abel"
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+const shortGreeting = "Welcome, " + currentUser.slice(0, 1) + "!"
