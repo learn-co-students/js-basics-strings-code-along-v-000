bcd670221413384b19fe8571d3fa616ad7f19e35
nichia <nichia@gmail.com>
2019-01-07 13:45:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+let currentUser = "The 'currentUser' variable must contain a string";
+
+currentUser = "You need to modify the value of the 'currentUser' variable";
+
+let welcomeMessage = "Welcome to Flatbook, ";
+
+welcomeMessage = "Welcome to Flatbook, " + currentUser;
+
+welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+
+excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+ + currentUser.toUpperCase();
+
+excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+ + currentUser.toUpperCase() + "!";
+
+ let shortGreeting = "Welcome, ";
+ shortGreeting = "Welcome, " + currentUser[0];
+
+ shortGreeting = "Welcome, " + currentUser[0] + "!";
