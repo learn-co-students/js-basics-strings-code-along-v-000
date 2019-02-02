72a1125345dd8f62f903b59a1e7a6065a36316ca
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-02 01:17:18 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 
 excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
