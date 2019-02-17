5c0520afc66f233459e767c7b471bb5bee9d3d16
Mohammed Noori <kmanoorib@gmail.com>
2019-02-17 04:30:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser = "Edsger Dijkstra";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+let firstInitial = currentUser[0];
+
+let shortGreeting = "Welcome, " + firstInitial + "!";
+
+
