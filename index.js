e3a35f78bc6a138e7a9b357dc25b5dcbf24cf01a
Catherine Bronzo <cbronzo@mac.com>
2019-03-12 23:48:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+let currentUser = "Catherine";
+
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, CATHERINE!";
+
+const shortGreeting = "Welcome, " + currentUser[0] + "!"
