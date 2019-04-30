d30ff860560817f74f8af784ddc391ba93ea3614
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-04-30 17:17:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'string';
+const welcomeMessage = 'Welcome to Flatbook, string!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, STRINGs!";
+const shortGreeting = 'Welcome, s!';
