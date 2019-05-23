9bb75ad1c2589955d0b720e98a0a0d7b9a34b79f
sarawall1187 <sarawall1187@gmail.com>
2019-05-23 10:45:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Edsger Dijkstra';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
