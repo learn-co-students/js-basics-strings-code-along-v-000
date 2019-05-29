a6056459d79361dceb575f8a5534de9ebcf7e012
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-29 00:01:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = "Wagyu Beef"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const firstInitial = currentUser.slice(0,1)
+
+const shortGreeting = `Welcome,  ${firstInitial}!`
