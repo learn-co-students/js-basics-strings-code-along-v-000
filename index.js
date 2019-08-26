53065ff20b0181ffa310098cfe9a0c3ea3395ee6
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 15:12:52 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const currentUser = 'John Smith';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const firstInitial = currentUser[0];
-const shortGreeting = 'Welcome, ' + currentUser[0] + '!';
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
