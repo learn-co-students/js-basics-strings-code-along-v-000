8603a75dedb9df0de9e6aefe8cb7c8c7f623eb7f
Sushi Chanrai <sushi@pathworks.co>
2019-09-30 16:02:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Sushi';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
