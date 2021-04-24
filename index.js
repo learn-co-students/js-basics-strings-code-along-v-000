c19202c44fc2885d53726d737f0a42cdd2e8fd31
CharlesOK <okwechimeamechi@gmail.com>
2021-04-24 01:09:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = "Willie Wonka";
-let welcomeMessage = `Welcome to Flatbook,   ${currentUser} + !`;
+let welcomeMessage = `Welcome to Flatbook,   ${currentUser}!`;
 let excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}`;
-let shortGreeting = `Welcome,   ${currentUser.charAt(0)} + !`;
+let shortGreeting = `Welcome,   ${currentUser.charAt(0)}!`;
